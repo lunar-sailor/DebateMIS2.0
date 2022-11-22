39027992ffb8fbe4cce09c16e3c8511095d38e91
lunar-sailor <1162214327@qq.com>
2022-11-22 22:44:36 +0800

    提交框架

---
+++ src/main/java/lunar_sailor/debatemis/DebateMisApplication.java
@@ -0,0 +1,13 @@
+package lunar_sailor.debatemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateMisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebateMisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lunar_sailor/debatemis/DebateMisApplicationTests.java
@@ -0,0 +1,13 @@
+package lunar_sailor.debatemis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateMisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
